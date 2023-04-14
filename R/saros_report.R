246b19c712b59ae4d10dcc7c0f40e9460809e999
Stephan Daus <stephan.daus@gmail.com>
2023-04-14 14:04:23 +0200

    0.0.5

---
+++ R/saros_report.R
@@ -0,0 +1,53 @@
+render_saros_report <-
+  function(chapter_overview,
+           data,
+           path = getwd(),
+           yaml_path = fs::path(path, "_metadata.yml")
+  ) {
+    check_data_frame(chapter_overview)
+    check_data_frame(data)
+    check_string(yaml_path, n=1, null.ok=FALSE)
+    if(!fs::is_file(yaml_path, follow = TRUE)) {
+      cli::cli_abort("{.arg {yaml_path}} is not a valid file path.")
+    }
+
+    yml <- yaml::yaml.load_file(yaml_path)
+    yml_element_args <- yml$element_args
+    # Must add all missing entries by using defaults?
+
+    data_overview <-
+      refine_data_overview(data_overview = chapter_overview,
+                           data = data,
+                           group_by = yml$group_by,
+                           label_separator = yml$label_separator,
+                           name_separator = yml$name_separator,
+                           sort_by = yml$sort_by)
+
+
+    elements_list <-
+      yml$elements_vector %>%
+      rlang::set_names() %>%
+      purrr::imap(.f = ~{
+
+        lst_saros_elements(data_overview = data_overview,
+                           element_name = rlang::set_names(.x, .y),
+                           data = data,
+                           label_separator = yml$label_separator,
+                           !!!yml_element_args)
+      })
+    report_filepath <-
+      gen_qmd_report(data_overview = data_overview,
+                   elements = elements_list,
+                   glue_index_string = yml$glue_index_string,
+                   show_if_alpha_below = yml$show_if_alpha_below,
+                   captions = yml$captions,
+                   report_ymlthis_config = yml$report_ymlthis_config,
+                   chapter_ymlthis_config = yml$chapter_ymlthis_config,
+                   index_filename = yml$index_filename,
+                   path = path)
+    if(interactive()) {
+      quarto::quarto_render(report_filepath)
+      utils::browseURL(url = report_filepath)
+    }
+    report_filepath
+  }
