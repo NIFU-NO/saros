df32b9cc788472a6c58f9a63069d8504779bed6e
sda030 <stephus.daus@gmail.com>
2023-11-22 21:44:44 +0100

    First go at checkbox_plot

---
+++ R/checkbox_prop_plot_html.R
@@ -0,0 +1,275 @@
+#'
+#' Create Single Interactive Categorical Plot with Univariates for Categorical Columns Sharing Same Categories
+#'
+#' @inheritParams draft_report
+#' @inheritParams summarize_data
+#' @inheritParams gen_qmd_chapters
+#' @param inverse Flag, defaults to FALSE. If TRUE, swaps x-axis and faceting.
+#'
+#' @importFrom rlang !!!
+#'
+#' @return ggiraph object, plottable with plot()
+#' @keywords internal
+prep_checkbox_prop_plot_html <-
+  function(data,
+           ...,
+           colour_2nd_binary_cat = NULL,
+           inverse = FALSE,
+           call = rlang::caller_env()) {
+
+
+    dots <- update_dots(dots = rlang::list2(...),
+                        caller_function = "checkbox_prop_plot")
+
+
+
+    if(is.null(colour_2nd_binary_cat)) {
+      colour_2nd_binary_cat <- "#090909"
+    }
+
+    indep_vars <- colnames(data)[!colnames(data) %in%
+                                   .saros.env$summary_data_sort2]
+    hide_axis_text <-
+      isTRUE(dots$hide_axis_text_if_single_variable) &&
+      length(indep_vars) == 0 &&
+      dplyr::n_distinct(data[[".variable_label"]]) == 1
+
+    hide_legend <- TRUE
+    colour_palette <- rlang::set_names(c(NA, colour_2nd_binary_cat),
+                                       nm = levels(data[[".category"]]))
+
+
+    max_nchar_cat <- max(nchar(levels(data[[".category"]])), na.rm = TRUE)
+
+    percentage <- dots$data_label %in% c("percentage", "percentage_bare")
+    prop_family <- dots$data_label %in% c("percentage", "percentage_bare", "proportion")
+
+    p <-
+      data %>%
+      dplyr::mutate(
+        .id = seq_len(nrow(.)),
+        .category = factor(.data[[".category"]], levels = rev(levels(.data[[".category"]]))),
+        .data_label = ifelse(as.character(.data[[".category"]]) == names(colour_palette)[1],
+                             NA, .data[[".data_label"]]),
+        .variable_label = forcats::fct_reorder(.data[[".variable_label"]],
+                                               .data[[".mean"]]),
+        .tooltip = # Tooltip contains all data except variable name
+                sprintf(fmt = stringi::stri_c("%s",
+                                            "N = %.0f",
+                                            stringi::stri_c("P = %.", dots$digits, "f%%", ignore_null=TRUE),
+                                            "%s",
+                                          sep="\n", ignore_null = TRUE),
+                    .data[[".category"]],
+                    .data[[".count"]],
+                    .data[[".proportion"]]*100,
+                    .data[[".variable_label"]]),
+        .tooltip = ifelse(rlang::is_string(indep_vars),
+                          yes = sprintf(fmt = stringi::stri_c("%s", "%s", sep="\n", ignore_null = TRUE),
+                                  .data[[".tooltip"]],
+                                  .data[[indep_vars]]),
+                          no = .data[[".tooltip"]]),
+        .onclick = sprintf(fmt = stringi::stri_c("%s", "Variable: %s", sep="\n", ignore_null = TRUE),
+                           .data[['.tooltip']], .data[['.variable_name']]),
+        .onclick = paste0('alert(\"', .data[['.onclick']], '\");'),
+        .onclick = stringi::stri_replace_all_regex(.data[[".onclick"]],
+                                                   pattern = "\n",
+                                                   replacement = "\\\\n")
+      ) %>%
+      ggplot2::ggplot(
+        mapping = ggplot2::aes(
+          y = .data[[if (prop_family) ".proportion" else stringi::stri_c(ignore_null=TRUE, ".", dots$data_label)]],
+          x = if(length(indep_vars) == 1 && isFALSE(inverse)) .data[[indep_vars]] else .data[[".variable_label"]],
+          fill = .data[[".category"]],
+          group = .data[[".category"]],
+          label = .data[[".data_label"]],
+          data_id = .data[[".id"]],
+          onclick = .data[[".onclick"]]
+        ),
+        cumulative = TRUE
+      ) +
+      ggiraph::geom_col_interactive(
+        mapping = ggplot2::aes(tooltip = .data[[".tooltip"]]), # BUG: Messes up order of categories if enabled.
+        position = ggplot2::position_stack(reverse = TRUE),
+        na.rm = TRUE
+      ) +
+      ggiraph::geom_text_interactive(
+        mapping = ggplot2::aes(
+          colour =
+            ggplot2::after_scale(x = hex_bw(.data$fill,
+              colour_2nd_binary_cat = colour_2nd_binary_cat
+            ))
+        ),
+        position = ggplot2::position_stack(vjust = .5, reverse = TRUE),
+        show.legend = FALSE, na.rm = TRUE
+      ) +
+      ggplot2::scale_y_continuous(
+        limits = c(-.003, if (prop_family) 1.015 else NA),
+        expand = c(0, 0),
+        labels = if (percentage) function(x) stringi::stri_c(ignore_null=TRUE, x * 100, "%") else ggplot2::waiver()
+      ) +
+      ggiraph::scale_fill_manual_interactive(
+        name = "",
+        values = colour_palette,
+        data_id = function(x) x,
+        tooltip = function(x) x,
+        drop = FALSE
+      ) +
+      ggiraph::scale_colour_manual_interactive(guide = FALSE, values = c("black", "white")) +
+      ggplot2::scale_x_discrete(limits = rev, labels = function(x) string_wrap(x, width = dots$x_axis_label_width)) +
+      ggplot2::guides(
+        fill = "none",
+        colour = "none"
+      ) +
+      ggplot2::theme_classic() +
+      ggplot2::theme(
+        text = ggiraph::element_text_interactive(family = dots$font_family),
+        axis.text.y = if (hide_axis_text) ggplot2::element_blank() else ggiraph::element_text_interactive(data_id = "axis.text.y"),
+        plot.caption = ggiraph::element_text_interactive(data_id = "plot.caption", size = dots$main_font_size),
+        legend.position = "bottom",
+        legend.justification = if(!rlang::is_string(indep_vars)) c(-.1, 0) else c(-.3, 0),
+        legend.direction = "horizontal",
+        legend.text = ggiraph::element_text_interactive(data_id = "legend.text", size = dots$main_font_size),
+        strip.placement = "outside",
+        strip.text.x = ggplot2::element_text(margin = ggplot2::margin(l = 0, t = 0, r = 0, b = 2, "cm")),
+        strip.text.y.left = ggiraph::element_text_interactive(data_id = "strip.text", angle = dots$strip_angle,
+                                                              hjust = .5, size = dots$main_font_size), # if(length(indep_vars)>0) ggplot2::element_blank() else
+        strip.background = ggiraph::element_rect_interactive(colour = NA)
+      ) +
+      ggplot2::labs(x = NULL, y = NULL, fill = NULL)
+
+    if (length(indep_vars) > 1L || (length(indep_vars) >= 1L && dplyr::n_distinct(data[[".variable_label"]]) > 1)) {
+      if(!inverse) {
+      p <- p +
+        ggiraph::facet_grid_interactive(
+          rows = ggplot2::vars(.data[[".variable_label"]]),
+          labeller = ggiraph::labeller_interactive(
+            .mapping = ggplot2::aes(
+              data_id = .data[[".variable_label"]],
+              tooltip = .data[[".variable_label"]],
+              label = string_wrap(.data$.variable_label,
+                                  width = dots$x_axis_label_width
+              )
+            )
+          ),
+          interactive_on = "text",
+          switch = "y", scales = "free_y", space = "free_y"
+        )
+      } else {
+        p <- p +
+          ggiraph::facet_grid_interactive(
+            rows = ggplot2::vars(.data[[indep_vars]]),
+            labeller = ggiraph::labeller_interactive(
+              .mapping = ggplot2::aes(
+                data_id = .data[[indep_vars]],
+                tooltip = .data[[indep_vars]],
+                label = string_wrap(.data[[indep_vars]],
+                                    width = dots$x_axis_label_width
+                )
+              )
+            ),
+            interactive_on = "text",
+            switch = "y", scales = "free_y", space = "free_y"
+          )
+      }
+    }
+
+    if (!dots$vertical) {
+      p + ggplot2::coord_flip()
+    } else {
+      p
+    }
+  }
+
+
+
+#' Embed Interactive Categorical Plot
+#'
+#'
+#' @inheritParams draft_report
+#' @inheritParams summarize_data
+#' @inheritParams gen_qmd_chapters
+#' @param colour_palette Character vector of colour codes.
+#' @param inverse Flag, defaults to FALSE. If TRUE, swaps x-axis and faceting.
+#' @param html_interactive *Toggle interactive plot*
+#'
+#'   `scalar<logical>` // *default:* `TRUE` (`optional`)
+#'
+#'   Whether plot is to be interactive (ggiraph) or static (ggplot2).
+#'
+#' @return ggplot
+#' @importFrom rlang !!!
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' embed_checkbox_prop_plot(data = ex_survey1, dep = b_1:b_3)
+#' }
+embed_checkbox_prop_plot <-
+  function(data,
+           ...,
+           dep = tidyselect::everything(),
+           indep = NULL,
+           mesos_group = NULL,
+           inverse = FALSE,
+           call = rlang::caller_env()) {
+
+    if(interactive()) cli::cli_inform("Have you remembered to recode missing on all variables in battery if none of them have been checked?")
+
+    dots <- update_dots(dots = rlang::list2(...),
+                        caller_function = "checkbox_prop_plot")
+
+
+    check_multiple_indep(data, indep = {{ indep }}, call = call)
+
+    dep_enq <- rlang::enquo(arg = dep)
+    dep_pos <- tidyselect::eval_select(dep_enq, data = data, error_call = call)
+    indep_enq <- rlang::enquo(arg = indep)
+    indep_pos <- tidyselect::eval_select(indep_enq, data = data, error_call = call)
+
+    check_category_pairs(data = data, cols_pos = c(dep_pos))
+
+    data_out <-
+      rlang::exec(
+        summarize_data,
+        data = data,
+        dep = names(dep_pos),
+        indep = names(indep_pos),
+        # add_n_to_bygroup = TRUE,
+        !!!dots
+      )
+
+
+    if (length(indep_pos) > 0) {
+      data_out[[names(indep_pos)]] <- forcats::fct_rev(data_out[[names(indep_pos)]])
+    }
+
+    if (dplyr::n_distinct(data_out[[".category"]], na.rm = dots$showNA == "never") == 2 &&
+      is_colour(dots$colour_2nd_binary_cat)) {
+      data_out$.category <- forcats::fct_rev(data_out$.category)
+    } else cli::cli_abort("{.arg dep} {names(dep_pos)} do(es) not contain two categories (excluding missing), or is missing {.arg colour_2nd_binary_cat} (set to {colour_2nd_binary_cat}).")
+
+    chart <-
+      rlang::exec(
+        prep_checkbox_prop_plot_html,
+        data = data_out,
+        inverse = inverse,
+        colour_2nd_binary_cat = dots$colour_2nd_binary_cat,
+        !!!dots[!names(dots) %in% "colour_2nd_binary_cat"]
+      )
+
+    if(!rlang::is_null(dots$label_separator)) {
+      indep_label <- unname(get_raw_labels(data = data, col_pos = indep_pos))
+      attr(chart, "saros_caption") <-
+        get_raw_labels(data = data, col_pos = dep_pos) %>%
+        get_main_question2(label_separator = dots$label_separator) %>%
+        create_caption(
+          data_out = data_out,
+          indep_pos = indep_label,
+          mesos_group = mesos_group,
+          filepath = NULL,
+          translations = dots$translations
+        )
+    }
+
+    chart
+  }
---
+++ R/prepare_chunk.uni_checkbox_prop_plot.R
@@ -0,0 +1,73 @@
+prepare_chunk.uni_checkbox_prop_plot <-
+  function(chapter_overview_section,
+           data,
+           mesos_group=NULL,
+           filepaths,
+           obj_name,
+           colour_palette,
+           plot_height=15,
+           ...) {
+
+    dots <- rlang::list2(...)
+
+    if(!all(chapter_overview_section$.variable_type_dep %in% c("fct", "ord")) ||
+       !all(is.na(as.character(chapter_overview_section$.variable_name_indep))) ||
+       length(unique(unlist(data[, unique(as.character(chapter_overview_section$.variable_name_dep)), drop=FALSE]))) > 3) return()
+
+    if(all(chapter_overview_section$.element_name == "uni_checkbox_prop_plot")) {
+        embed_checkbox_plot_docx <- embed_checkbox_prop_plot_docx
+        embed_checkbox_plot <- embed_checkbox_prop_plot
+        element_name_snippet <- "uni_checkbox_prop_plot_html"
+    } else {
+      embed_checkbox_plot_docx <- embed_checkbox_freq_plot_docx
+      embed_checkbox_plot <- embed_checkbox_freq_plot
+      element_name_snippet <- "uni_checkbox_freq_plot_html"
+
+      }
+
+    out_docx <-
+      rlang::exec(
+        embed_checkbox_plot_docx,
+        data = data,
+        dep = unique(as.character(chapter_overview_section$.variable_name_dep)),
+        colour_palette = colour_palette,
+        mesos_group = mesos_group,
+        plot_height = plot_height,
+        !!!dots)
+    print(out_docx, target = filepaths$abs$docx)
+
+    out_html <-
+      rlang::exec(
+        embed_checkbox_plot,
+        data = data,
+        dep = unique(as.character(chapter_overview_section$.variable_name_dep)),
+        colour_palette = colour_palette,
+        mesos_group = mesos_group,
+        html_interactive = TRUE,
+        !!!dots)
+    ggplot2::ggsave(plot = out_html,
+                    filename = filepaths$abs$png,
+                    scale = dots$png_scale,
+                    width = dots$png_width,
+                    height = dots$png_height,
+                    units = "cm", dpi = "retina")
+    writexl::write_xlsx(x = out_html$data, filepaths$abs$xlsx)
+    qs::qsave(out_html, file = filepaths$abs$rds)
+
+
+    out <-
+      c(insert_obj_in_qmd(element_name = element_name_snippet,
+                        index = obj_name,
+                        mesos_group = mesos_group,
+                        filepath = filepaths$rel$rds,
+                        figure_height = plot_height,
+                        max_width_obj = dots$max_width_obj,
+                        max_width_file = dots$max_width_file,
+                        translations = dots$translations,
+                        caption = attr(out_html, "saros_caption")))
+    stringi::stri_c(out, collapse="\n", ignore_null=TRUE)
+
+  }
+
+prepare_chunk.uni_checkbox_freq_plot <- prepare_chunk.uni_checkbox_prop_plot
+
