ce5daa0b74966a00196f6c05cfc892f755f46628
sda030 <stephus.daus@gmail.com>
2023-11-24 18:50:44 +0100

    adjusted estimate_plot_height's legend height

--- R/estimate_plot_height.R
+++ R/estimate_plot_height.R
@@ -1,94 +1,3 @@
-# estimate_plot_height <- function(data,
-#                                  y_col_pos,
-#                                  x_cols = NULL,
-#                                  label_separator = " - ",
-#                                  vertical = FALSE,
-#                                  x_axis_label_width = 20,
-#                                  element_name = "uni_cat_prop_plot",
-#                                  showNA = "never",
-#                                  plot_height_multiplier_per_horizontal_line = .2,
-#                                  plot_height_multiplier_per_vertical_letter = .15,
-#                                  plot_height_fixed_constant = 1,
-#                                  plot_height_max = 8,
-#                                  plot_height_min = 1,
-#                                  vertical_height = 12,
-#                                  strip_angle = 0
-#                                  ) {
-#
-#   if(!vertical) {
-#
-#     max_axis_lines_per_dep <- get_raw_labels(data = data, col_pos = y_col_pos)
-#     max_axis_lines_per_dep <- keep_subitem(max_axis_lines_per_dep, label_separator = label_separator)
-#     max_axis_lines_per_dep <- stringi::stri_wrap(max_axis_lines_per_dep,
-#                                                  width = x_axis_label_width,
-#                                                  simplify = FALSE)
-#     # if(any(stringr::str_detect(y_col_pos, "d_"))) browser()
-#
-#
-#
-#     n_cats <- dplyr::n_distinct(as.vector(as.matrix(data[, y_col_pos])), na.rm = showNA == "never")
-#     multi_line_legend <- ceiling(n_cats/5)
-#     if( !stringi::stri_detect(element_name, fixed="freq")) {
-#       n_cats <- 1
-#     }
-#     x_max <- if(length(x_cols)>0 && all(!is.na(x_cols))) {
-#
-#       dplyr::n_distinct(data[[x_cols]], na.rm = TRUE)
-#       }
-#
-#     if(rlang::is_null(x_cols) || all(is.na(x_cols))) { # Must include NA due to new chapter_overview setup
-#
-#       max_axis_lines_per_dep <- max(lengths(max_axis_lines_per_dep), na.rm=TRUE)
-#
-#
-#     plot_height_estimate <-
-#       max_axis_lines_per_dep *
-#       n_cats *
-#       length(y_col_pos) *
-#       plot_height_multiplier_per_horizontal_line +
-#       plot_height_fixed_constant +
-#       multi_line_legend
-#
-#     # Bivariate plots, with dep as vertical facet
-#     } else if(strip_angle >= 45 && strip_angle <= 135) {
-#
-#       max_axis_lines_per_dep <- max(nchar(max_axis_lines_per_dep), na.rm=TRUE)
-#
-#       deps_as_dominant <-
-#         max_axis_lines_per_dep *
-#         plot_height_multiplier_per_vertical_letter
-#
-#       indeps_as_dominant <-
-#         x_max *
-#         n_cats *
-#         plot_height_multiplier_per_horizontal_line
-#
-#       plot_height_estimate <-
-#         max(c(deps_as_dominant, indeps_as_dominant)) *
-#         length(y_col_pos) +
-#         plot_height_fixed_constant +
-#         multi_line_legend
-#
-#     } else { # Bivariate plots, with dep as facet horizontally
-#
-#       max_axis_lines_per_dep <- max(lengths(max_axis_lines_per_dep), na.rm=TRUE)
-#
-#       plot_height_estimate <-
-#         max(c(max_axis_lines_per_dep, x_max * n_cats)) *
-#         length(y_col_pos) *
-#         plot_height_multiplier_per_horizontal_line +
-#         plot_height_fixed_constant +
-#         multi_line_legend
-#     }
-#
-#     plot_height <-
-#       max(c(min(c(plot_height_estimate, plot_height_max)),
-#             plot_height_min))
-#
-#   } else plot_height <- vertical_height
-#   plot_height
-# }
-
 estimate_plot_height <- function(data,
                                  y_cols,
                                  x_cols = NULL,
@@ -114,13 +23,18 @@ estimate_plot_height <- function(data,
 
   calculate_height <- function(max_lines, y_n_cats,
                                y_n_cols, multiplier, fixed, legend_lines) {
-    max_lines * y_n_cats * y_n_cols * multiplier + fixed + legend_lines
+    max_lines * y_n_cats * y_n_cols * multiplier + legend_lines * 1.1 + fixed
   }
 
   if (!is_vertical) {
     max_lines <- get_max_lines(data, y_cols, x_axis_label_width, label_separator)
-    y_n_cats <- dplyr::n_distinct(as.vector(as.matrix(data[, y_cols])), na.rm = showNA == "never")
+    unique_y_cats <- unique(as.vector(as.matrix(data[, y_cols])))
+    if(showNA == "never") unique_y_cats <- unique_y_cats[!is.na(unique_y_cats)]
+    y_n_cats <- length(unique_y_cats)
+    max_nchar_cat <- max(nchar(unique_y_cats))
     legend_lines <- ceiling(y_n_cats / 5)
+    legend_lines <- max(c(ceiling(y_n_cats / 5),
+                          (max_nchar_cat > 10)+1), na.rm = TRUE)
 
     if (!stringi::stri_detect(element_name, fixed = "freq")) {
       y_n_cats <- 1
