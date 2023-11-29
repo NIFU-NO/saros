d7080c998527236c41b526f2d70c5e5524686528
sda030 <stephus.daus@gmail.com>
2023-11-29 21:15:12 +0100

    Deduplicates colons in sanitize_labels

--- R/qualtrics.R
+++ R/qualtrics.R
@@ -106,6 +106,7 @@ sanitize_labels <- function(data, sep = " - ", multi_sep_replacement = ": ",
       if(stringi::stri_count_fixed(label, " - ")>=2) label <- stringi::stri_replace_first_fixed(label, pattern = sep, replacement = multi_sep_replacement)
       if(stringi::stri_count_fixed(label, " - ")>=2) label <- stringi::stri_replace_first_fixed(label, pattern = sep, replacement = multi_sep_replacement)
       label <- stringi::stri_replace_all_regex(label, pattern = "^[[:space:]]|[[:space:]-:\\.]+$", replacement = "")
+      label <- stringi::stri_replace_all_regex(label, pattern = ":+", replacement = ":")
 
       attr(data[[var]], "label") <- label
     }
