6d8ce2044f985c10cfd1d71ba862bfcd14613029
sda030 <stephus.daus@gmail.com>
2023-12-01 10:14:11 +0100

    Fixed chapter_cleaning - now only chapters are abbreviated. Also informs user

--- R/filename_sanitizer.R
+++ R/filename_sanitizer.R
@@ -4,7 +4,7 @@ filename_sanitizer <- function(x, max_clean_folder_name = 8) {
                                     pattern = "[[:punct:][:space:]]",
                                     replacement = "_")
   out <- iconv(out, from ="UTF-8", to="windows-1250")
-  out <- stringi::stri_sub(out, from = 1, to = max_clean_folder_name)
+
 
   # out <- make.names(out)
   out
