b7b2595bd5af7f9cfe22d6469886e110388e1fd2
sda030 <stephus.daus@gmail.com>
2023-12-12 14:12:10 +0100

    updated ex_survey_chapter_overview

--- data-raw/create_ex_survey_ch_overview.rds.R
+++ data-raw/create_ex_survey_ch_overview.rds.R
@@ -3,10 +3,11 @@ ex_survey_ch_overview <-
    chapter = c("Introduction",
                "Ambivalence",
                "Big mysteries",
-               "Confidence"),
-   author = c("Ernst Hemmingway", "Agatha Christie", "Mark Twain", "Stephen King"),
-   dep = c("", "matches('^c_'), a_1:a_6, a_9", "p_1:p_4, b_1:b_3", "starts_with('d_'), matches('^e_')"),
-   indep = c("", "x1_sex", "x2_human", "f_uni"),
+               "Confidence",
+               "Doubt"),
+   author = c("Ernst Hemmingway", "Agatha Christie", "Mark Twain", "Stephen King", NA),
+   dep = c("", "matches('^c_'), a_1:a_6, a_9", "p_1:p_4, b_1:b_3", "starts_with('d_'), matches('^e_')", "open_comments"),
+   indep = c("", "x1_sex", "x3_nationality", "f_uni", ""),
    irrelevant_col = "abc")
-saveRDS(ex_survey_ch_overview, file = paste0("inst", .Platform$file.sep, "extdata", .Platform$file.sep, "ex_survey_ch_overview.RDS"), compress = FALSE)
+saveRDS(ex_survey_ch_overview, file = file.path("inst", "extdata", "ex_survey_ch_overview.RDS"), compress = FALSE)
 usethis::use_data(ex_survey_ch_overview, overwrite = TRUE)
