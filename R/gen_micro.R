c0bec889ac390f778ab828c265b6c4fd65f2be01
sda030 <stephus.daus@gmail.com>
2024-01-17 16:10:23 +0100

    Minor license and roxygen2 fixes2

--- R/gen_micro.R
+++ R/gen_micro.R
@@ -3,7 +3,7 @@ gen_micro <- function(data, cols, path) {
   dir.create(path, "Micro")
   utils::write.csv2(data, file=file.path(path, "Micro", "micro.csv"))
   writexl::write_xlsx(data, path=file.path(path, "Micro", "micro.xlsx"))
-  qs::qsave(data, file = file.path(path, "Micro", "micro.Rds"))
+  qs::qsave(data, file = file.path(path, "Micro", "micro.qs"))
 
 
   # Codebook
