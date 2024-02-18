4d65cde8f1c72a2a1a144687286b127a7d6810da
sda030 <stephus.daus@gmail.com>
2024-02-18 16:09:13 +0100

    feat: `setup_access_restrictions` now accepts NULL password in password prompt. Simply cancel or click escape when password input pops up.

--- R/setup_access_restrictions.R
+++ R/setup_access_restrictions.R
@@ -35,6 +35,7 @@ refer_main_password_file <- function(x=".main_htpasswd_private",
                                     log_rounds = 12,
                                     append_users = FALSE,
                                     password_input = c("prompt", "8", "10", "12", "16")) {
+
   password_input <- rlang::arg_match(password_input, multiple = FALSE)
   # Read in x, split it into usernames and plaintext passwords, encrypt the passwords, return a table
   if(!file.exists(x)) {
@@ -49,12 +50,17 @@ refer_main_password_file <- function(x=".main_htpasswd_private",
     if(!rlang::is_string(passwd) || nchar(passwd)==0) {
       if(isFALSE(append_users)) cli::cli_abort("Unable to find password for username {user}.")
       if(password_input == "prompt") {
+
         passwd <- rstudioapi::askForPassword(prompt = stringi::stri_c("Enter password for new user: ", user))
+        if(is.null(passwd)) return(NULL)
+
       } else {
         if(length(password_input)>1 ||
            is.na(as.integer(password_input)) ||
            as.integer(password_input)<1) {
+
           cli::cli_abort("Password input must be a positive integer stored as string.")
+
         }
         passwd <- sample(x = c(letters, LETTERS, 0:9),
                          size = as.integer(password_input),
---
+++ tests/testthat/test-setup_access_restrictions.R
@@ -0,0 +1,14 @@
+testthat::test_that("refer_main_password_file", {
+
+  if(interactive()) {
+  file <- tempfile(fileext = ".htpasswd_private")
+  saros:::write_htpasswd_file(x=data.frame(username="test", password="test"), file = file)
+  saros:::refer_main_password_file(x=file, usernames="test2", append_users = TRUE, password_input = "prompt") |>
+    testthat::expect_equal(expected = NULL)
+
+
+  }
+  saros:::refer_main_password_file(x=file, usernames="test3", append_users = TRUE, password_input = "12") |>
+    testthat::expect_type("character")
+
+})
