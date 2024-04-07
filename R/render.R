65b8bf340a3cd235d1579f6ab920aa71748b33a9
sda030 <stephus.daus@gmail.com>
2024-04-07 13:03:17 +0200

    -feat: Removed unnecessary rendering-assistance tools.

--- R/render.R
+++ R/render.R
@@ -44,116 +44,11 @@ copy_content <- function(from_files, from_folders, to_folder) {
 }
 
 
-# Function to create directories if they don't exist
-ensure_dir_exists <- function(dir_path) {
-  if (!dir.exists(dir_path)) {
-    dir.create(dir_path, recursive = TRUE)
-  }
-}
-
-#' Simple wrapper for quarto::quarto_render() that temporarily sets LC_ALL="C" and takes the processing time
-#'
-#' @param site_path String, path to render.
-#' @param ... Dynamic dots forwarded to quarto::quarto_render
-#'
-#' @return Nothing
-#' @export
-quarto_render_saros <- function(site_path,
-                                ...) {
-
-  dots <- rlang::list2(...)
-  # Render the site using Quarto
-  old_locale <- Sys.getlocale("LC_ALL")
-  old_wd <- getwd()
-  # on.exit(setwd(site_path), add = TRUE)
-  on.exit(Sys.setenv(LC_ALL = old_locale), add = TRUE)
-  Sys.setenv(LC_ALL = "C")
-
-  # Execute the rendering
-  system.time(
-    quarto::quarto_render(
-      input = site_path,
-      # as_job = FALSE,
-      output_format = c("all"),
-      !!!dots
-    )
-  )
-
-  utils::browseURL(site_path)
-
-  invisible()
-}
-
-
-
-#' Convenience function to prepare, copy and render website
-#'
-#' Rendering a website on a Sharepoint/OneDrive location can be a pain
-#' due to long filepaths above 260 characters, which causes read errors. This
-#' function simplifies things by copying a site from local_basepath to a site_basepath,
-#' sets up basic access authentication files (either Netlify's _headers file or
-#' regular Apache .htaccess/.htpasswd files), as well as optionally copying
-#' in an existing Netlify _publish.yaml-file.
-#'
-#' @param remote_basepath String. FTP servers often have a subdomain path location different from "/home/". Adjust this here.
-#' @param local_basepath String. Path to where your QMD-files are located (the site to be rendered).
-#' @param site String. Path to where to copy folders and files to setup what Quarto needs to build a project.
-#' @param rel_path_base_to_parent_of_user_restricted_folder String. Path going from basepath to the folder containing folders to password-protect.
-#' @param from_folders,from_files Character vector of folders and files to copy into the site path.
-#' @param overwrite Flag. Defaults to FALSE to ensure you know what you are doing. If TRUE, will delete all files and folders in site!
-#' @param prompt Flag. Whether to ask the user if they are certain. Defaults to TRUE.
-#'
-#' @return local_basepath
-#' @export
-#'
-prepare_safe_render <- function(remote_basepath = "/home/",
-                                 from_folders = NULL,
-                                 from_files = NULL,
-                                 local_basepath,
-                                 site,
-                                 rel_path_base_to_parent_of_user_restricted_folder = "Reports",
-
-                                 overwrite = FALSE,
-                                 prompt = TRUE) {
-
-  # dots <- rlang::list2(...)
-
-  # Initial checks of folders
-  if(!dir.exists(local_basepath) || length(dir(local_basepath))==0) {
-    cli::cli_abort("Nothing to render: Reports directory is missing/empty: {.path {local_basepath}}.")
-  }
-
-  if(is.character(rel_path_base_to_parent_of_user_restricted_folder)) {
-    local_mesos_paths <- file.path(local_basepath, rel_path_base_to_parent_of_user_restricted_folder)
-      if(any(!dir.exists(local_mesos_paths)) || any(lengths(dir(local_mesos_paths))==0)) {
-        local_mesos_paths <- local_mesos_paths[!dir.exists(local_mesos_paths) | lengths(dir(local_mesos_paths))==0]
-        cli::cli_abort("Nothing to render: Reports directory is missing/empty: {.path {local_mesos_paths}}.")
-      }
-  }
-  ensure_dir_exists(site)
-
-  # Emptying folders, or keeping them if freeze/cache, then copying files. EXPERIMENTAL
-  if(prompt) {
-    answer <- utils::menu(graphics = TRUE,
-                          title = "Sure you want to overwrite the local site folder {.path {site}}?",
-                          choices = c("Y", "N")) # Default to "No"
-    if (toupper(answer) == "N") return()
-  }
-
-  if(isTRUE(overwrite)) {
-    unlink(site, recursive = TRUE)
-  }
-  copy_content(from_folders = from_folders,
-               from_files = from_files,
-               to_folder = site)
-
-}
-
-
-
 #' Create Data Frame Containing Email Drafts with User Credentials
 #'
-#' @inheritParams prepare_safe_render
+#' @param local_basepath Local path for project, typically project directory.
+#' @param rel_path_base_to_parent_of_user_restricted_folder Character vector
+#' of path(s) to the parent folder where the mesos reports can be found.
 #' @param email_data_frame Data.frame/tibble with (at least) emails and usernames
 #' @param email_col String, name of email column
 #' @param username_col String, name of username column in email_data_frame
