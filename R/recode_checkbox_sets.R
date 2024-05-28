76ab4af4839c640137e673117ca0048bdd8c22d8
sda030 <stephus.daus@gmail.com>
2024-05-28 18:05:10 +0200

    -feat: recode_checkbox_sets minor update

--- R/recode_checkbox_sets.R
+++ R/recode_checkbox_sets.R
@@ -1,10 +1,10 @@
 #' Recodes Multiple Sets of Factor Variables of Checkbox Items
 #'
-#' Qualtrics produces datasets where a matrix set of checkbox questions 
+#' Qualtrics produces datasets where a matrix set of checkbox questions
 #' have value labels (factor labels) representing the actual question. When presenting
-#' these questions in a figure or table, however, it is often useful to recode 
+#' these questions in a figure or table, however, it is often useful to recode
 #' the value labels to something common, e.g. "Selected" and "Not selected".
-#' 
+#'
 #' Furthermore, this function recodes all of the variables within a set to missing
 #' if all of the variables are missing - under the assumption that the respondent
 #' has then omitted them all. In other cases, e.g. one checkbox is checked, then
@@ -13,18 +13,21 @@
 #' @param data Data frame or tibble.
 #' @param var_patterns Character vector of regex search patterns that uniquely identify
 #' variables belonging to sets of batteries. Same as used in tidyselect's matches().
-#' @param keep_tmp_var Flag, whether the temporary variables created should 
+#' @param keep_tmp_var Flag, whether the temporary variables created should
 #' @param input_check_value,input_uncheck_value Factor/integer values indicating checked and unchecked responses.
 #' @param output_check_value,output_uncheck_value Recoded values for checked and unchecked responses.
 #' @param output_all_unchecked_value If respondent has not checked any checkboxes, what should they be recoded as? Set to NULL to not recode.
-#' @param output_check_label,output_uncheck_label,output_all_unchecked_label 
+#' @param output_check_label,output_uncheck_label,output_all_unchecked_label
 #'   Factor labels.
+#' @param convert_to_factor Flag. Whether to convert variables to factor (using `output_*_label` args) or leave as is.
 #' @param keep_tmp_var Whether to keep temporary variables for output_all_unchecked_value (mostly for debugging). Defaults to FALSE.
 #' @return Data frame/tibble.
+#' @importFrom rlang `:=`
 #' @export
 #'
 #' @examples
-#' n <- 100 
+#' n <- 100
+#' set.seed(1241)
 #' tmp <-
 #' data.frame(a_1 = factor(sample(c("Car", NA), size = n, replace = TRUE)),
 #'            a_2 = factor(sample(c("Train", NA), size = n, replace = TRUE)),
@@ -36,10 +39,10 @@
 #'            b_4 = factor(sample(c("Grapes", NA), size = n, replace = TRUE)))
 #' tmp2 <- recode_checkbox_sets(tmp, var_patterns = c("^a_", "^b_[[:digit:]]$"))
 #' cbind(tmp, tmp2)
-#'                      
-recode_checkbox_sets <- 
-  function(data, 
-           var_patterns = NULL, 
+#'
+recode_checkbox_sets <-
+  function(data,
+           var_patterns = NULL,
            input_check_value = 1L,
            input_uncheck_value = NA_integer_,
            output_check_value = 2L,
@@ -50,7 +53,7 @@ recode_checkbox_sets <-
            output_all_unchecked_label = NULL,
            convert_to_factor = TRUE,
            keep_tmp_var = FALSE) {
-    
+
     if(!inherits(data, "data.frame")) cli::cli_abort("{.arg data} must be a data frame or tibble.")
 
     checker <- function(arg) {
@@ -63,11 +66,11 @@ recode_checkbox_sets <-
                  output_check_label, output_uncheck_label, output_all_unchecked_label)) {
       checker(arg = arg)
     }
-    
-    
+
+
     data_out <- data
     for(var_pattern in var_patterns) {
-      tmp_var <- stringi::stri_replace_all_regex(str = var_pattern, 
+      tmp_var <- stringi::stri_replace_all_regex(str = var_pattern,
                                                  pattern = "[^[:alnum:]]+", replacement = "_")
       tmp_var <- paste0("tmp_", tmp_var, "_tmp")
       data_out <-
@@ -75,21 +78,21 @@ recode_checkbox_sets <-
         dplyr::rowwise() |>
         dplyr::mutate("{tmp_var}" := sum(as.integer(dplyr::c_across(tidyselect::matches(var_pattern))), na.rm=TRUE)) |>
         dplyr::ungroup() |>
-        dplyr::mutate(dplyr::across(tidyselect::matches(var_pattern), 
-                                    ~dplyr::case_when(if(!is.null(output_all_unchecked_value)) .data[[tmp_var]] == 0 ~ output_all_unchecked_value, 
+        dplyr::mutate(dplyr::across(tidyselect::matches(var_pattern),
+                                    ~dplyr::case_when(if(!is.null(output_all_unchecked_value)) .data[[tmp_var]] == 0 ~ output_all_unchecked_value,
                                                       as.integer(.x) %in% input_uncheck_value ~ output_uncheck_value,
                                                       .default = output_check_value))) |>
         dplyr::ungroup()
       if(isFALSE(keep_tmp_var)) {
         data_out[[tmp_var]] <- NULL
       }
-      
+
       if(isTRUE(convert_to_factor)) {
         data_out <-
           data_out |>
           dplyr::mutate(dplyr::across(tidyselect::matches(var_pattern),
-                                      ~factor(.x, 
-                                              levels = c(output_uncheck_value, output_check_value, output_all_unchecked_value), 
+                                      ~factor(.x,
+                                              levels = c(output_uncheck_value, output_check_value, output_all_unchecked_value),
                                               labels = c(output_uncheck_label, output_check_label, output_all_unchecked_label))))
       }
     }
