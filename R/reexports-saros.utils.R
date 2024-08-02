e087b2ff3c7d688e19c9d669a22ef85d6ebcfed3
sda030 <stephus.daus@gmail.com>
2024-08-02 10:17:14 +0200

    Re-exports
    Closes #333

---
+++ R/reexports-saros.utils.R
@@ -0,0 +1,80 @@
+
+#' @importFrom saros.utils attach_qualtrics_labels
+#' @export
+saros.utils::attach_qualtrics_labels
+
+#' @importFrom saros.utils copy_folder_contents_to_dir
+#' @export
+saros.utils::copy_folder_contents_to_dir
+
+#' @importFrom saros.utils create_directory_structure
+#' @export
+saros.utils::create_directory_structure
+
+#' @importFrom saros.utils create_email_credentials
+#' @export
+saros.utils::create_email_credentials
+
+#' @importFrom saros.utils create_r_files
+#' @export
+saros.utils::create_r_files
+
+#' @importFrom saros.utils download_zip_to_folder
+#' @export
+saros.utils::download_zip_to_folder
+
+#' @importFrom saros.utils generate_yaml_from_directory
+#' @export
+saros.utils::generate_yaml_from_directory
+
+#' @importFrom saros.utils initialize_saros_project
+#' @export
+saros.utils::initialize_saros_project
+
+#' @importFrom saros.utils omitted_recoder_df
+#' @export
+saros.utils::omitted_recoder_df
+
+#' @importFrom saros.utils post_render_docx_img_replacer
+#' @export
+saros.utils::post_render_docx_img_replacer
+
+#' @importFrom saros.utils read_default_draft_report_args
+#' @export
+saros.utils::read_default_draft_report_args
+
+#' @importFrom saros.utils recode_checkbox_sets
+#' @export
+saros.utils::recode_checkbox_sets
+
+#' @importFrom saros.utils remove_special_chars_in_labels
+#' @export
+saros.utils::remove_special_chars_in_labels
+
+#' @importFrom saros.utils rename_by_labels
+#' @export
+saros.utils::rename_by_labels
+
+#' @importFrom saros.utils replace_docx_imgs_with_mscharts
+#' @export
+saros.utils::replace_docx_imgs_with_mscharts
+
+#' @importFrom saros.utils replace_stata_labels
+#' @export
+saros.utils::replace_stata_labels
+
+#' @importFrom saros.utils sanitize_labels
+#' @export
+saros.utils::sanitize_labels
+
+#' @importFrom saros.utils setup_access_restrictions
+#' @export
+saros.utils::setup_access_restrictions
+
+#' @importFrom saros.utils swap_label_colnames
+#' @export
+saros.utils::swap_label_colnames
+
+#' @importFrom saros.utils write_default_draft_report_args
+#' @export
+saros.utils::write_default_draft_report_args
