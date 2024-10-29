e98176a379c3a1c76ce397515d38cdcb666ce718
Stephan Daus <stephus.daus@gmail.com>
2024-10-29 21:34:53 +0100

    Updated test for crosstable with dep-indep all NA

--- tests/testthat/test-crosstable.R
+++ tests/testthat/test-crosstable.R
@@ -1,19 +1,22 @@
 testthat::test_that("crosstable", {
-library(saros)
+  library(saros)
   testthat::expect_no_error(object = {
     x <-
       ex_survey |>
       saros:::crosstable.data.frame(
         dep = paste0("b_", 1:3),
-        indep = NULL, showNA = "ifany")
+        indep = NULL, showNA = "ifany"
+      )
   })
-  testthat::expect_equal(dim(x), c(9,10))
-  testthat::expect_equal(object =
-                           x |>
-                           dplyr::filter(.variable_name == "b_1", .category == "A bit") |>
-                           dplyr::pull(.proportion),
-                         expected = 0.4766667,
-                         tolerance = .00001)
+  testthat::expect_equal(dim(x), c(9, 10))
+  testthat::expect_equal(
+    object =
+      x |>
+        dplyr::filter(.variable_name == "b_1", .category == "A bit") |>
+        dplyr::pull(.proportion),
+    expected = 0.4766667,
+    tolerance = .00001
+  )
 
   testthat::expect_no_error(object = {
     x <-
@@ -21,18 +24,22 @@ library(saros)
       saros:::crosstable.data.frame(
         dep = paste0("b_", 1:3),
         indep = "x1_sex",
-        showNA = "ifany")
+        showNA = "ifany"
+      )
   })
   testthat::expect_equal(dim(x), c(18, 11))
-  testthat::expect_equal(object =
-                           x |>
-                           dplyr::filter(.variable_name == "b_1",
-                                         .category == "A lot",
-                                         x1_sex == "Males") |>
-                           dplyr::pull(.proportion),
-                         expected = 0.1059603,
-                         tolerance = .0001)
-
+  testthat::expect_equal(
+    object =
+      x |>
+        dplyr::filter(
+          .variable_name == "b_1",
+          .category == "A lot",
+          x1_sex == "Males"
+        ) |>
+        dplyr::pull(.proportion),
+    expected = 0.1059603,
+    tolerance = .0001
+  )
 })
 
 
@@ -42,11 +49,31 @@ testthat::test_that("crosstable works with explicit NA level", {
   testthat::expect_no_error(object = {
     x <-
       ex_survey |>
-      dplyr::mutate(dplyr::across(p_1:p_4, ~forcats::fct_na_value_to_level(.x))) |>
-      saros:::crosstable.data.frame(dep = paste0("p_", 1:4),
-                                             indep = "x1_sex", showNA = "ifany")
+      dplyr::mutate(dplyr::across(p_1:p_4, ~ forcats::fct_na_value_to_level(.x))) |>
+      saros:::crosstable.data.frame(
+        dep = paste0("p_", 1:4),
+        indep = "x1_sex", showNA = "ifany"
+      )
   })
-  testthat::expect_equal(dim(x), c(34,11))
+  testthat::expect_equal(dim(x), c(34, 11))
 })
 
 
+
+testthat::test_that("crosstable works with dep and indep having nothing in common and showNA='never'", {
+  library(saros)
+
+  testthat::expect_no_error(object = {
+    x <-
+      tibble::tibble(.rows = 100) |>
+      dplyr::mutate(
+        A = factor(sample(c(letters[1:5], NA), replace = T, size = 100)),
+        B = factor(ifelse(!is.na(A), NA, sample(letters[1:2], replace = TRUE, size = 100)))
+      ) |>
+      saros:::crosstable.data.frame(
+        dep = "A",
+        indep = "B", showNA = "never"
+      )
+  })
+  testthat::expect_equal(dim(x), c(1, 11))
+})
