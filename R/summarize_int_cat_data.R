e79dd4a4a2e5b0ab597d39d0340bf38b06570566
Stephan Daus <stephus.daus@gmail.com>
2025-01-09 22:45:20 +0100

    refactoring and expanding simple_descriptives

--- R/summarize_int_cat_data.R
+++ R/summarize_int_cat_data.R
@@ -1,34 +1,39 @@
 summarize_int_cat_data <-
   function(data,
-           ...,
            dep = colnames(data),
            indep = NULL,
+           ...,
            call = rlang::caller_env()) {
-
     dots <- rlang::list2(...)
-
-    if(!(inherits(data, what = "data.frame") || !inherits(data, what = "survey"))) {
+    if (!(inherits(data, what = "data.frame") || !inherits(data, what = "survey"))) {
       cli::cli_abort("{.arg data} should be a data.frame/tibble or survey object, not {.obj_type_friendly {data}}.")
     }
 
-    if(any(dep %in% indep)) {
+    if (any(dep %in% indep)) {
       cli::cli_abort("Dep column{?s} {.var {invalid_deps}} {?is/are} among indep columns.")
     }
     invalid_deps <- dep[!dep %in% colnames(data)]
-    if(length(invalid_deps)>0) {
+    if (length(invalid_deps) > 0) {
       cli::cli_abort("Column{?s} {.var {invalid_deps}} {?doesn't/don't} exist.")
     }
     invalid_indeps <- indep[!indep %in% colnames(data)]
-    if(length(invalid_indeps)>0) {
+    if (length(invalid_indeps) > 0) {
       cli::cli_abort("Column{?s} {.var {invalid_indeps}} {?doesn't/don't} exist.")
     }
 
-
-    # for(d in dep) {
-    #   simple_descriptives(data = data,
-    #                       y_var = dep,
-    #                       x_var = indep,
-    #                       na.rm = dots$showNA %in% c("ifany", "always"))
-    # }
-
+    if (length(indep) <= 1) {
+      simple_descriptives(
+        data = data,
+        y_var = dep,
+        x_var = indep
+      )
+    } else {
+      lapply(indep, function(i) {
+        simple_descriptives(
+          data = data,
+          y_var = dep,
+          x_var = i
+        )
+      }) |> dplyr::bind_rows(.id = indep)
+    }
   }
--- tests/testthat/test-simple_desciptives.R
+++ tests/testthat/test-simple_desciptives.R
@@ -17,35 +17,35 @@ testthat::test_that("simple_descriptives removes NA in x_var if na.rm = TRUE", {
   data <- data.frame(y_var = c(1, 2, 3, 4, 5), x_var = c("A", NA, "B", "B", "B"))
   result <- saros:::simple_descriptives(data = data, y_var = "y_var", x_var = "x_var", na.rm = TRUE)
   testthat::expect_true(is.data.frame(result))
-  testthat::expect_true(ncol(result)==7)
+  testthat::expect_equal(ncol(result), 11)
 })
 
 testthat::test_that("simple_descriptives does not remove NA in x_var if na.rm = FALSE", {
   data <- data.frame(y_var = c(1, 2, 3, 4, 5), x_var = c("A", NA, "B", "B", "B"))
   result <- saros:::simple_descriptives(data = data, y_var = "y_var", x_var = "x_var", na.rm = FALSE)
   testthat::expect_true(is.data.frame(result))
-  testthat::expect_true(ncol(result)==7)
-  testthat::expect_true(nrow(result)==3)
+  testthat::expect_equal(ncol(result), 11)
+  testthat::expect_equal(nrow(result), 3)
 })
 
 testthat::test_that("simple_descriptives returns data frame if y_var is categorical", {
   data <- data.frame(y_var = c("a", "b", "c"), x_var = c("A", "B", "B"))
   result <- saros:::simple_descriptives(data = data, y_var = "y_var", x_var = "x_var", table_wide = F)
   testthat::expect_true(is.data.frame(result))
-  testthat::expect_equal(dim(result), c(2,3))
+  testthat::expect_equal(dim(result), c(2, 3))
 })
 
 testthat::test_that("simple_descriptives handles single row input", {
   data <- data.frame(y_var = c(1))
   result <- saros:::simple_descriptives(data = data, y_var = "y_var")
   testthat::expect_true(is.data.frame(result))
-  testthat::expect_true(result$mean==1)
+  testthat::expect_equal(result$mean, 1)
 })
 testthat::test_that("simple_descriptives handles single row input", {
   data <- data.frame(y_var = c(1), x_var = c("A"))
   result <- saros:::simple_descriptives(data = data, y_var = "y_var", x_var = "x_var")
   testthat::expect_true(is.data.frame(result))
-  testthat::expect_true(result$mean==1)
+  testthat::expect_equal(result$mean, 1)
 })
 
 testthat::test_that("simple_descriptives handles missing y_var gracefully", {
